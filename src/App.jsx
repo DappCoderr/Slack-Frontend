d6a68a64386246361905188ec65cf73b3f9db16b
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2025-12-22 17:33:48 +0530

    add Query client

--- src/App.jsx
+++ src/App.jsx
@@ -3,31 +3,32 @@ import Auth from '@/pages/Auth/Auth';
 import SignInCard from '@/components/organisms/Auth/SignInCard';
 import SignUpCard from '@/components/organisms/Auth/signUpCard';
 import NotFound from '@/pages/NotFound';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 function App() {
+  const queryClient = new QueryClient();
   return (
-    <Routes>
-      <Route
-        path="/auth/signin"
-        element={
-          <Auth>
-            <SignInCard />
-          </Auth>
-        }
-      />
-      <Route
-        path="/auth/signup"
-        element={
-          <Auth>
-            <SignUpCard />
-          </Auth>
-        }
-      />
-      <Route
-        path="*"
-        element={<NotFound/>}
-      />
-    </Routes>
+    <QueryClientProvider client={queryClient}>
+      <Routes>
+        <Route
+          path="/auth/signin"
+          element={
+            <Auth>
+              <SignInCard />
+            </Auth>
+          }
+        />
+        <Route
+          path="/auth/signup"
+          element={
+            <Auth>
+              <SignUpCard />
+            </Auth>
+          }
+        />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </QueryClientProvider>
   );
 }
 
