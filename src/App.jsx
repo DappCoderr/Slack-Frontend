322c710d18a08eecc140dd2e22a6e74e0eb158ed
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2025-12-24 15:16:04 +0530

    added custom hook for useAuth

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <AppContextProvider>
-        <AppRoutes/>
+        <AppRoutes />
         <Toaster />
       </AppContextProvider>
     </QueryClientProvider>
--- src/utils/combineContext.jsx
+++ src/utils/combineContext.jsx
@@ -6,4 +6,4 @@ const combineContext = (...providers) => {
   };
 };
 
-export default combineContext
+export default combineContext;
