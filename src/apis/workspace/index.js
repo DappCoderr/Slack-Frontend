2ef8dfa651303f628a3a4ca3166a6df0f7a70fa4
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2026-01-01 16:32:43 +0530

    fix: add optional chaining to response

--- src/apis/workspace/index.js
+++ src/apis/workspace/index.js
@@ -12,7 +12,7 @@ export const createWorkspaceRequest = async ({ name, description, token }) => {
       }
     );
     console.log('Response in creating workspace request: ', response);
-    return response.data;
+    return response?.data?.data;
   } catch (error) {
     console.log('Error in creating workspace request: ', error);
     throw error?.response?.data;
@@ -27,7 +27,7 @@ export const fetchWorkspaceRequest = async ({ token }) => {
       },
     });
     console.log('Response in fetch workspace request: ', response);
-    return response.data.data;
+    return response?.data?.data;
   } catch (error) {
     console.log('Error in fetching workspace request: ', error);
     throw error?.response?.data;
