178a85738cdba39f880d469310d6caba329594b0
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2026-01-02 17:08:48 +0530

    update: add workspace navbar

--- src/pages/Workspace/Layout.jsx
+++ src/pages/Workspace/Layout.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import WorkspaceSidebar from '@/components/organisms/Workspace/WorkspaceSidebar';
+import WorkspaceNavbar from '@/components/organisms/Workspace/WorkspaceNavbar';
 
 const WorkspaceLayout = ({ children }) => {
   return (
     <div className="h-[100vh]">
-      <div className="flex h-[calc(100vh-10px)]">
+      <WorkspaceNavbar />
+      <div className="flex h-[calc(100vh-40px)]">
         <WorkspaceSidebar />
+        Workspace
         {children}
       </div>
     </div>
