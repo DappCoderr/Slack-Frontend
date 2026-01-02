e63177f2c9fad558911b3369abf6f74c5e16ceea
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2026-01-02 17:07:05 +0530

    add: workspace navbar

---
+++ src/components/organisms/Workspace/WorkspaceNavbar.jsx
@@ -0,0 +1,35 @@
+import { Button } from '@/components/ui/button';
+import { useGetWorkspaceById } from '@/hooks/apis/workspace/useGetWorkspaceById';
+import { InfoIcon, LucideLoader2, SearchIcon } from 'lucide-react';
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+const WorkspaceNavbar = () => {
+  const { workspaceId } = useParams();
+  //   const { isLoading, workspaces } = useGetWorkspaceById();
+
+  if (false) {
+    return <LucideLoader2 className="animate-spin mt-2" />;
+  }
+  return (
+    <nav className="flex items-center justify-center h-10 p-1.5 bg-slack-Dark">
+      <div className="flex-1" />
+      <div>
+        <Button
+          size="sm"
+          className="bg-accent/25 hover:bg-accent/15 w-full justify-start h-7 px-2"
+        >
+          <SearchIcon className="size-5 text-white mr-2" />
+          <span className="text-white text-xs">Search Workspace</span>
+        </Button>
+      </div>
+      <div className="ml-auto flex-1 flex items-center justify-end">
+        <Button variant="transparent" size="iconSm">
+          <InfoIcon className="size-5 text-white" />
+        </Button>
+      </div>
+    </nav>
+  );
+};
+
+export default WorkspaceNavbar;
