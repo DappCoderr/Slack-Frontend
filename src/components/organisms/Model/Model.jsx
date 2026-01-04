ce011eb08f9541eed0e0ba28d0da746b009a20ed
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2026-01-04 21:36:37 +0530

    update: WorkspacePrefrenceModal to Modal

--- src/components/organisms/Model/Model.jsx
+++ src/components/organisms/Model/Model.jsx
@@ -1,10 +1,12 @@
 import CreateWorkspaceModel from '@/components/molecules/CreateWorkspaceModel/CreateWorkspaceModel';
+import WorkspacePrefrenceModal from '@/components/molecules/Workspace/WorkspacePrefrenceModal';
 import React from 'react';
 
 const Model = () => {
   return (
     <>
       <CreateWorkspaceModel />
+      <WorkspacePrefrenceModal />
     </>
   );
 };
