b5a4775fe07baa4b823e7d40202eb1adef838d3a
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2026-01-04 21:35:19 +0530

    add: WorkspacePrefrencesModalProvider to AppContext

--- src/context/AppContextProvider.jsx
+++ src/context/AppContextProvider.jsx
@@ -1,8 +1,10 @@
 import combineContext from '@/utils/combineContext';
 import { AuthContextProvider } from './AuthContext';
 import { CreateWorkspaceContextProvider } from './CreateWorkspaceContext';
+import { WorkspacePrefrencesModalProvider } from './WorkspacePrefrencesModal';
 
 export const AppContextProvider = combineContext(
   AuthContextProvider,
-  CreateWorkspaceContextProvider
+  CreateWorkspaceContextProvider,
+  WorkspacePrefrencesModalProvider
 );
