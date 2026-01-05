0b4ff34ffa58a912501d5eb340afa4d37d3985c7
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2026-01-05 19:46:17 +0530

    fix: added workspace name

--- src/components/molecules/Workspace/WorkspacePrefrenceModal.jsx
+++ src/components/molecules/Workspace/WorkspacePrefrenceModal.jsx
@@ -26,12 +26,10 @@ const WorkspacePrefrenceModal = () => {
         <div className="px-4 pb-4 flex flex-col gap-y-2">
           <div className="px-5 py-4 bg-white rounded-lg border cursor-pointer hover:bg-gray-50">
             <div className="flex items-center justify-between">
-              <p className="font-semibold text-xs">
-                Workspace Name
-              </p>
+              <p className="font-semibold text-xs">Workspace Name</p>
               <p className="font-semibold text-xs hover:underline">Edit</p>
             </div>
-            <p className='font-semibold text-xs'>{initialValue}</p>
+            <p className="font-semibold text-xs">{initialValue}</p>
           </div>
           <Button>
             <TrashIcon />
