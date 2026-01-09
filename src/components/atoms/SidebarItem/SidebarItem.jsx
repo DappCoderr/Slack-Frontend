31bccb7bb2a46c10c8d881d51a5e52e6ab4f1f82
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2026-01-09 18:37:47 +0530

    create sidebar item atom

---
+++ src/components/atoms/SidebarItem/SidebarItem.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import { cva } from 'class-variance-authority';
+import { Link, useParams } from 'react-router-dom';
+
+import { Button } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
+
+const sidebarItemVariable = cva('flex items-center jutify-start gap-1.5 font-normal px-[20px] text-sm overflow-hidden w-full', {
+  variants: {
+    variant: {
+      default: 'text-[#f9edffcc]',
+      active: 'text-[#481350] bg-white/90 hover:bg-white/80',
+    },
+  },
+  defaultVariants: 'default',
+});
+
+const SidebarItem = ({ label, id, icon: Icon, variant }) => {
+  const { workspaceId } = useParams();
+  return (
+    <Button variant='transparent' size='sm' className={cn(sidebarItemVariable({ variant }))}>
+      <Link className='flex items-center gap-1.5' to={`/workspaces/${workspaceId}/channels/${id}`}>
+        <Icon className='size-3.5 mr-1' />
+        <span className='text-sm'>{label}</span>
+      </Link>
+    </Button>
+  );
+};
+
+export default SidebarItem;
