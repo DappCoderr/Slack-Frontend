88a9e3c998f10a3c46280522e0a777dd1afcc526
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2026-01-09 16:16:07 +0530

    create channel context

---
+++ src/context/CreateChannelContext.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+
+import { createContext } from 'react-router-dom';
+
+const CreateChannelContext = createContext();
+
+export const CreateChannelContextProvider = ({ children }) => {
+  const [openCreateChannelModal, setOpenCreateChannelModal] = useState(second);
+  return (
+    <CreateChannelContext.Provider value={{ openCreateChannelModal, setOpenCreateChannelModal }}>
+        {children}
+    </CreateChannelContext.Provider>
+  )
+};
+
+export default CreateChannelContext;
