58d2c899453763881a3570dce548d566b9ee18c2
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2026-01-10 16:16:00 +0530

    add channel to workspace api to channel modal

--- src/components/molecules/CreateChannelModal/CreateChannelModal.jsx
+++ src/components/molecules/CreateChannelModal/CreateChannelModal.jsx
@@ -1,20 +1,41 @@
 import React, { useState } from 'react';
 
+import { useQueryClient } from '@tanstack/react-query';
+
 import { Button } from '@/components/ui/button';
-import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from '@/components/ui/dialog';
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from '@/components/ui/dialog';
 import { Input } from '@/components/ui/input';
+import { useAddChannelToWorkspace } from '@/hooks/apis/workspace/useAddChannelToWorkspace';
 import useCreateChannelModal from '@/hooks/context/useCreateChannelModal';
+import useCurrentWorkspace from '@/hooks/context/useCurrentWorkspace';
+import { toast } from 'sonner';
 
 const CreateChannelModal = () => {
   const { openCreateChannelModal, setOpenCreateChannelModal } = useCreateChannelModal();
-  const [channelName, setChannelName] = useState();
+  const { isPending, addChannelToWorkspaceMutation } = useAddChannelToWorkspace();
+  const [channelName, setChannelName] = useState('');
+
+  const { currentWorkspace } = useCurrentWorkspace();
+
+  const queryClient = useQueryClient();
 
   const handleClose = () => {
     setOpenCreateChannelModal(false);
   };
 
-  const handleFormSubmit = (e) => {
+  const handleFormSubmit = async (e) => {
     e.preventDefault();
+    try {
+      await addChannelToWorkspaceMutation({ workspaceId: currentWorkspace?._id, channelName: channelName });
+      toast.success("Channel created successfully")
+      queryClient.invalidateQueries(`fetchWorkspaceById-${currentWorkspace?._id}`);
+    } catch (error) {
+      console.log('Not able to add channel to workspace: ', error);
+      toast.error("Channel creation failed")
+    } finally {
+      setOpenCreateChannelModal(false);
+      setChannelName('');
+    }
   };
 
   return (
@@ -28,7 +49,9 @@ const CreateChannelModal = () => {
           <Input required minLength={3} placeholder='Channel name (e.g. Welcome)' value={channelName} onChange={(e) => setChannelName(e.target.value)} />
 
           <div className='flex justify-end mt-4'>
-            <Button type='submit'>Create</Button>
+            <Button disabled={isPending} type='submit'>
+              Create
+            </Button>
           </div>
         </form>
       </DialogContent>
