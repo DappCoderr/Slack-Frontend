14c45c6c35c12224db5af2937d632ab070b10574
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2026-01-10 16:14:34 +0530

    add workspace context

--- src/context/AppContextProvider.jsx
+++ src/context/AppContextProvider.jsx
@@ -3,11 +3,13 @@ import combineContext from '@/utils/combineContext';
 import { AuthContextProvider } from './AuthContext';
 import { CreateChannelContextProvider } from './CreateChannelContext';
 import { CreateWorkspaceContextProvider } from './CreateWorkspaceContext';
+import { WorkspaceContextProvider } from './WorkspaceContext';
 import { WorkspacePrefrencesModalContextProvider } from './WorkspacePrefrencesModalContext';
 
 // prettier-ignore
 export const AppContextProvider = combineContext(
     AuthContextProvider, 
+    WorkspaceContextProvider,
     CreateWorkspaceContextProvider, 
     WorkspacePrefrencesModalContextProvider,
     CreateChannelContextProvider
