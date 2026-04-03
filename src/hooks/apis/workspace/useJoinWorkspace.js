61e329abbe51400e7ec7727f5f9c00068dadbfad
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2026-04-03 21:01:09 +0530

    refactor: restructure the code

--- src/hooks/apis/workspace/useJoinWorkspace.js
+++ src/hooks/apis/workspace/useJoinWorkspace.js
@@ -1,16 +1,32 @@
-import React from 'react'
-import { joinWorkspaceRequest } from '@/apis/workspace'
-import useAuth from '@/hooks/context/useAuth'
-import { useMutation } from '@tanstack/react-query'
+import React from 'react';
+
+import { useMutation } from '@tanstack/react-query';
+
+import { joinWorkspaceRequest } from '@/apis/workspace';
+import useAuth from '@/hooks/context/useAuth';
 
 export const useJoinWorkspace = (workspaceId, joinCode) => {
-    const {auth} = useAuth
-    const { isPending, error, isSuccess, mutateAsync: joinWorkspaceMutation} = useMutation({
-        mutationFn: () => joinWorkspaceRequest({workspaceId, joinCode, token: auth?.token})
-    })
-    return{
-        isPending,
-        isSuccess,
-        joinWorkspaceMutation
-    }
-}
+  const { auth } = useAuth();
+  const {
+    isPending,
+    error,
+    isSuccess,
+    mutateAsync: joinWorkspaceMutation,
+  } = useMutation({
+    mutationFn: (joinCode) => {
+        console.log("Token:", auth?.token)
+        return joinWorkspaceRequest({workspaceId, joinCode, token: auth?.token})
+    } ,
+    onSuccess: () => {
+      console.log('Workspace joined successfully');
+    },
+    onError: (error) => {
+      console.log('Error in joining workspace', error);
+    },
+  });
+  return {
+    isPending,
+    isSuccess,
+    joinWorkspaceMutation,
+  };
+};
