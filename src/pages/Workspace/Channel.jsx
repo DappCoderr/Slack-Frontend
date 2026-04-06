b9c408fed9f61e3c1e0296d38967b7dacb97779d
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2026-04-06 13:43:10 +0530

    fix: added channel details

--- src/pages/Workspace/Channel.jsx
+++ src/pages/Workspace/Channel.jsx
@@ -1,8 +1,35 @@
+import { Loader2Icon, TriangleAlertIcon } from 'lucide-react';
 import { useParams } from 'react-router-dom';
+
+import { useGetChannelById } from '@/hooks/apis/channel/useGetChannelById';
 
 const Channel = () => {
   const { channelId } = useParams();
-  return <h1>channel id: {channelId}</h1>;
+  const { channelDetails, isFetching, isError } = useGetChannelById(channelId);
+
+  if (isFetching) {
+    return (
+      <div className='flex h-full items-center justify-center'>
+        <Loader2Icon className='size-5 animate-spin text-muted-foreground' />
+      </div>
+    );
+  }
+
+  if (isError) {
+    return (
+      <div className='flex h-full items-center justify-center'>
+        <TriangleAlertIcon className='size-6 text-muted-foreground' />
+        <span className='text-sm text-muted-foreground'>Channel not found</span>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <h1>channel id: {channelDetails?._id}</h1>
+      <p>channel Name: {channelDetails?.name}</p>
+    </div>
+  );
 };
 
 export default Channel;
